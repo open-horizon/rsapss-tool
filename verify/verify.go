8d6740be3236413413f6fbaeb6df25184717649a
linggao <linggao@us.ibm.com>
2017-11-07 02:44:56 +0000

    Issue 393 - Use rsapss-tool for deployment signature verification in anax

--- verify/verify.go
+++ verify/verify.go
@@ -8,6 +8,7 @@ import (
 	"encoding/base64"
 	"encoding/pem"
 	"fmt"
+	"hash"
 	"io/ioutil"
 )
 
@@ -16,6 +17,8 @@ type KeyMapping struct {
 	PublicKeyPath string
 	Signature     string
 }
+
+const COMMON_ERROR = "COMMON_ERROR"
 
 func (m *KeyMapping) String() string {
 	return fmt.Sprintf("PublicKeyPath: %v, Signature: %v", m.PublicKeyPath, m.Signature)
@@ -48,31 +51,115 @@ func InputVerifiedByAll(keyMappings []*KeyMapping, input []byte) (bool, []*KeyMa
 	}
 
 	for _, keyMapping := range keyMappings {
-		pubkeyRaw, err := ioutil.ReadFile(keyMapping.PublicKeyPath)
+		signatureBytes, err := base64.StdEncoding.DecodeString(keyMapping.Signature)
 		if err != nil {
 			return false, failed, err
 		}
 
-		block, _ := pem.Decode(pubkeyRaw)
-		if block == nil {
-			return false, failed, fmt.Errorf("Unable to find PEM block in the provided public key: %v", keyMapping.PublicKeyPath)
-		}
-
-		pubkey, err := x509.ParsePKIXPublicKey(block.Bytes)
-		if err != nil {
-			return false, failed, err
-		}
-
-		signature, err := base64.StdEncoding.DecodeString(keyMapping.Signature)
-		if err != nil {
-			return false, failed, err
-		}
-
-		if err := rsa.VerifyPSS(pubkey.(*rsa.PublicKey), crypto.SHA256, hasher.Sum(nil), signature, nil); err != nil {
-			failed = append(failed, &KeyMapping{keyMapping.PublicKeyPath, keyMapping.Signature})
+		if ok, err := verify(keyMapping.PublicKeyPath, signatureBytes, hasher); !ok {
+			switch err.(type) {
+			case VerificationError:
+				failed = append(failed, &KeyMapping{keyMapping.PublicKeyPath, keyMapping.Signature})
+			default:
+				return false, failed, err
+			}
 		}
 	}
 
 	// if the signature was verified by all of the given keys we return true
 	return len(failed) == 0, failed, nil
+}
+
+// Verify the input with given signature and a list of public keys.
+// It returns true if any of the key verifies successfuly. The file name of the successful key is returned. (true, fn, nil).
+// It returns false if all keys failed. The errors are returned as a map keyed by the key file name. The common
+// errors are keyed by COMMON_ERROR. (false, "", map).
+func InputVerifiedByAnyKey(keyFiles []string, signature string, input []byte) (bool, string, map[string]error) {
+	failed := make(map[string]error)
+
+	// no input files, failed
+	if len(keyFiles) == 0 {
+		failed[COMMON_ERROR] = fmt.Errorf("No public key files provided; input not verified")
+		return false, "", failed
+	}
+
+	// hash input
+	hasher := sha256.New()
+	_, err := hasher.Write(input)
+	if err != nil {
+		failed[COMMON_ERROR] = fmt.Errorf("Error hashing input: %v, Error: %v", string(input), err)
+		return false, "", failed
+	}
+
+	// decode the signature into its binary form.
+	signatureBytes, err := base64.StdEncoding.DecodeString(signature)
+	if err != nil {
+		failed[COMMON_ERROR] = fmt.Errorf("Unable to base64 decode signature %v, error: %v", signature, err)
+		return false, "", failed
+	}
+
+	for _, keyFile := range keyFiles {
+		if ok, err := verify(keyFile, signatureBytes, hasher); !ok {
+			failed[keyFile] = err
+		} else {
+			return true, keyFile, nil
+		}
+	}
+
+	return false, "", failed
+}
+
+// Verification error
+type VerificationError struct {
+	Msg           string
+	InternalError error
+}
+
+// Error provides a loggable error message including the message of an
+// internal error (one enclosed in this error).
+func (e VerificationError) Error() string {
+	return fmt.Sprintf("%v. InternalError: %v", e.Msg, e.InternalError)
+}
+
+// Error found while checking the public key
+type KeyError struct {
+	Msg           string
+	InternalError error
+}
+
+// Error provides a loggable error message including the message of an
+// internal error (one enclosed in this error).
+func (e KeyError) Error() string {
+	return fmt.Sprintf("%v. InternalError: %v", e.Msg, e.InternalError)
+}
+
+// This function verifies the given signature and data with the public key.
+// It returns true if the verification is successful.
+// It returns false if the verification is not successful. The error indicates what went wrong.
+// The error can be VerificationError or KeyError
+func verify(keyFileName string, signatureBytes []byte, inputHash hash.Hash) (bool, error) {
+	// open the file
+	pubkeyRaw, err := ioutil.ReadFile(keyFileName)
+	if err != nil {
+		return false, KeyError{fmt.Sprintf("Unable to read key file: %v", keyFileName), err}
+	}
+
+	// check if the key is a public key
+	block, _ := pem.Decode(pubkeyRaw)
+	if block == nil {
+		return false, KeyError{fmt.Sprintf("Unable to find PEM block in the provided public key: %v", keyFileName), err}
+	}
+
+	pubkey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return false, KeyError{fmt.Sprintf("Unable to parse key file: %v, as a public key.", keyFileName), err}
+	}
+
+	// verify the signature
+	err = rsa.VerifyPSS(pubkey.(*rsa.PublicKey), crypto.SHA256, inputHash.Sum(nil), signatureBytes, nil)
+	if err != nil {
+		return false, VerificationError{fmt.Sprintf("Unable to verify signature using pubkey file: %v", keyFileName), err}
+	}
+
+	return true, nil
 }
